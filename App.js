665a984ae73fda0d78b12cd931ba62b6f33aaea7
ancient225 <91169811+ancient225@users.noreply.github.com>
2023-10-20 12:50:09 +1000

    testing
    started using the correct documentation - running into issue - testing with a file

--- App.js
+++ App.js
@@ -5,49 +5,61 @@ const responseTime = require('response-time')
 
 //set up systems
 const ffmpeg = require('ffmpeg');
-const command = ffmpeg();
 
 const app = express();
 app.use(responseTime());
 
+const axios = require('axios');
+const redis = require('redis');
+
+require('dotenv').config();
+const AWS = require('aws-sdk');
+
 //files
-const inputFilePath = 'C:\Users\timot\OneDrive\Desktop\Uni stuff\Cloud\GitHub\Cloud-Project\TestFile.mp4';
-const outputFilePath = 'C:\Users\timot\OneDrive\Desktop\Uni stuff\Cloud\GitHub\Cloud-Project\TestFile.mp4';
+const inputFilePath = 'TestFile.mp4';
+const outputFilePath = 'Outputvideos';
 
 
-//helpfull videos
-//https://www.youtube.com/watch?v=26Mayv5JPz0
-//https://ffmpeg.org/documentation.html
+//helpfull Links
+//https://www.npmjs.com/package/ffmpeg
 
 
 
 
 //test if everything set up
 app.get("/test", async (req, res) => {
-    
-    command.input(inputFilePath);
+    try {
+        //process video - not done (doesn't seem to have any data on the video or be able to save it)
+        var process = new ffmpeg(inputFilePath);
+        process.then(function (video) {
+            // Video metadata
+            console.log(video.metadata);
+            // FFmpeg configuration
+            console.log(video.info_configuration);
 
-    // Video codec, in this case, we'll use libx264 for MP4 format
-    command.videoCodec('libx264');
+            //Testing
+            video.setVideoFormat('avi');
+            video.save(outputFilePath, function (error, file) {
+                if (!error)
+                    console.log('Video file: ' + file);
+            });
+        }, function (err) {
+            console.log('Error: ' + err);
+        });
+    } catch (e) {
+        console.log(e.code);
+        console.log(e.msg);
+    }
 
-    // Audio codec
-    command.audioCodec('aac');
+    //edit video - not done
 
-    // Output file
-    command.output(outputFilePath);
+    //save video to redis - not done
 
-    // Run the FFmpeg command
-    command.on('end', () => {
-        res.json('Conversion finished');
-        console.log('Conversion finished');
-    })
-    .on('error', (err) => {
-        res.json('Error:');
-        console.error('Error:', err);
-    })
-    .run();
+    //save video to S3 - not done
 
-    
+    //return video - not done
+
+    res.json();  
 });
 
 app.listen(3000, () => {
